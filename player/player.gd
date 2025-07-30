88f59b841a5cebc7848b54702d011ba536cdf13c
vivithequeen <vivithequeen1@gmail.com>
2025-07-30 18:14:13 -0400

    started map

--- player/player.gd
+++ player/player.gd
@@ -1,9 +1,11 @@
 extends CharacterBody3D
 
 
-const SPEED = 12.0
-const JUMP_VELOCITY = 4.5
+const SPEED = 9.0
+const JUMP_VELOCITY = 6.0;
 const LOOK_SENSE = 0.0025;
+
+var controller_look_sense = 0.07;
 
 @onready var camera = $Camera3D
 
@@ -21,13 +23,28 @@ var dash_timer := 0.0;
 var dash_amount := 0.15;
 var dash_speed :=40.0;
 var dash_direction : Vector2
-
+var is_dashing:= false;
 var is_crouching = false
+var dashs = 3;
+
 func _ready() -> void:
 	update_mouse_mode()
 func _physics_process(delta: float) -> void:
+	$Camera3D/SubViewportContainer/SubViewport/Camera3D.global_position = global_position
+	$Camera3D/SubViewportContainer/SubViewport/Camera3D.global_rotation = global_rotation
 	# Add the gravity.
 	#headbob(delta);
+	var current_fov = 90;
+
+	
+	var look_dir = Input.get_vector("look_left","look_right","look_up","look_down")
+
+	if(look_dir):
+		rotation.y-=look_dir.x * controller_look_sense
+		camera.rotation.x-=look_dir.y * controller_look_sense
+		camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2);
+
+
 	crouch();
 	if not is_on_floor() and !dash_direction:
 		velocity += get_gravity() * delta
@@ -50,11 +67,16 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-	dash(delta,input_dir)
+	current_fov+=(5 if is_dashing else 0)
+	current_fov+=(5 if velocity != Vector3.ZERO else 0)
+	dash(delta,input_dir);
+	juice(delta,input_dir,current_fov);
 	move_and_slide()
 
 
 func _input(event: InputEvent) -> void:
+	
+	
 	if event is InputEventMouseMotion:
 		rotation.y +=(-event.relative.x * LOOK_SENSE);
 		camera.rotation.x+=(-event.relative.y*LOOK_SENSE)
@@ -67,12 +89,25 @@ func headbob(delta):
 	if (headbob_timer > PI):
 		headbob_timer = 0;
 
+
+func juice(delta, input_dir,current_fov):
+	var tween = get_tree().create_tween()
+	tween.tween_property(camera, "rotation:z",((deg_to_rad(-3) * input_dir.x) if input_dir else 0), 0.2)
+	tween.set_parallel()
+	tween.tween_property(camera, "fov", current_fov,0.2)
+
+
 func vec3tovec2(vec3 : Vector3) ->Vector2:
+	
 	return Vector2(vec3.x,vec3.z);
 	
 func dash(delta,input_dir):
-	if(Input.is_action_just_pressed("dash")):#start dash
+	if(is_on_floor()):
+		dashs = 3;
+	if(Input.is_action_just_pressed("dash") and !is_dashing and dashs > 0):#start dash
 		dash_timer = dash_amount;
+		dashs-=1;
+		is_dashing = true;
 		if(input_dir):
 			velocity.y = 0;
 			dash_direction = vec3tovec2((transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized());
@@ -82,13 +117,17 @@ func dash(delta,input_dir):
 		dash_timer-=delta
 		velocity.x =dash_direction.x * dash_speed
 		velocity.z =dash_direction.y * dash_speed
+	
 	else:
-		dash_direction = Vector2.ZERO
+		if(is_dashing):
+			is_dashing = false;
+			dash_direction = Vector2.ZERO
+
 
 
 
 func crouch():
-	is_crouching = Input.is_action_pressed("crouch");
+	is_crouching = Input.is_action_pressed("crouch") and is_on_floor();
 	if $crouchcast.is_colliding() and !is_crouching :
 		is_crouching = true
 	
--- player/weapons/weapon.gd
+++ player/weapons/weapon.gd
@@ -12,13 +12,10 @@ var fire_timer = 0;
 func _physics_process(delta: float) -> void:
 	if(fire_timer>=0):
 		fire_timer-=delta
-
-func _input(event: InputEvent) -> void:
-
-	if(event.is_action_pressed("fire") and fire_timer <= 0):
+	if(Input.is_action_pressed("fire") and fire_timer <= 0):
 		fire_timer = fire_rate;
 		var b := bullet_type.instantiate();
 		
 		b.spawn_rot = get_node("../../../../").global_rotation;
 		b.spawn_pos = get_node("../../../../bullet_spawn").global_position;
-		add_child(b)
+		get_node("../../../../../../").add_child(b)
